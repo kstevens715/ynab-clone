00fe11f7aeb43200f9f70acd35e26bfc78e33eba
Ben Kanouse <kanobt61@gmail.com>
2022-06-30 15:40:15 -0400

    Move sidebar javascript into a stimulus controller

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,27 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    this.element.draggable = true;
+    this.element.addEventListener('drag', this.resizeSidebar.bind(this))
+    this.element.addEventListener('drop', this.resizeSidebar.bind(this))
+  }
+
+  disconnect() {
+    this.element.removeEventListener('drag', this.resizeSidebar.bind(this))
+    this.element.removeEventListener('drop', this.resizeSidebar.bind(this))
+  }
+
+  resizeSidebar(event) {
+    const sidebar = this.element.previousElementSibling;
+    const rightX = event.clientX;
+    const leftX = sidebar.getBoundingClientRect().x;
+    const sidebarWidth = rightX - leftX;
+
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'none';
+
+    if (sidebarWidth > 50 && sidebarWidth < 700) {
+      sidebar.style.width = sidebarWidth + 'px';
+    }
   }
 }
