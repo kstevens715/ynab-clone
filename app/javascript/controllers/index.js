38ee7bf7a0e4f3b877c964b13006387d08830dba
Ben Kanouse <kanobt61@gmail.com>
2022-06-30 16:30:53 -0400

    Rename hello_controller to sidebar_resizer_controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import SidebarResizerController from "./sidebar_resizer_controller"
+application.register("sidebar-resizer", SidebarResizerController)
